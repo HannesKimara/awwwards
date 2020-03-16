cc9286efc43e809c915988c07e5a56894c80c065
Hannes Kimara <hanneskimara@gmail.com>
2020-03-16 22:02:23 +0300

    Implement authentication system

--- awwward/urls.py
+++ awwward/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/<str:username>', views.profile, name='profile'),
+    path('account', views.account, name='account'),
+    path('accounts/logout', views.logout_user, name="logout"),
 ]
--- awwward/views.py
+++ awwward/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, reverse
-
+from django.shortcuts import render, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 def index(request):
     return render(request, 'index.html', {'sites': [1, 2, 3, 4, 5, 6, 7, 7, 8, 9,10, 11]})
+
+
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
+@login_required(login_url='/accounts/login/')
+def account(request):
+    return render(request, 'account.html')
+
+
+def logout_user(request):
+    if request.user.is_authenticated:
+        logout(request)
+
+    return redirect('index')
